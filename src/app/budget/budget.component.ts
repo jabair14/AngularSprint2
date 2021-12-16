6d71b0e03a0612c58a5fdac429d4d62fece95640
Jabair <jabairk@gmail.com>
2021-12-16 15:29:11 -0500

    done

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -14,19 +14,48 @@ export class BudgetComponent implements OnInit {
     {name: "car", cost: 150},
     {name: "misc", cost: 0}
   ]
+  // rent = this.expenses[0].cost 
+  // phone = this.expenses[1].cost
+  // bills = this.expenses[2].cost
+  // car = this.expenses[3].cost
+  // misc = this.expenses[4].cost
 
    
   
 
   constructor() { }
+
+  ConvertToInt(val){
+    return parseInt(val);
+  }
     getTotalCost(){
-      let totalCost: number = this.expenses.map(
-        a => a.cost
-      ).reduce(function(a,b){
-        return a + b 
-      })
+      // let totalCost: number = this.expenses.map(
+      //   a => a.cost
+      //   ).reduce(function(a,b){
+      //     return a + b 
+      //   })
+      //   console.log(totalCost)
+      //   return totalCost
+      console.log(this.expenses[0].cost)
+      let totalCost: number = +(this.expenses[0].cost) + +(this.expenses[1].cost) + +(this.expenses[2].cost) + +(this.expenses[3].cost) + +(this.expenses[4].cost)
       return totalCost
     }
+
+    onChangeCost(event: any){
+      console.log('something')
+      let newCost = event.target.value
+      let originalExpense = this.expenses.find(
+        expense => expense.name == event.target.name
+        )
+      this.expenses.find(
+        expense => expense == originalExpense
+      ).cost = newCost
+      console.log(this.expenses)
+      // console.log(originalExpense)
+      
+      
+    }
+    
 
   ngOnInit(): void {
     
